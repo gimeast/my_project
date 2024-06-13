ac96851a84a7239e249e8f14cf2f35a324200ec7
gimeast <dudu.gimeast@gmail.com>
2024-06-13 23:56:30 +0900

    웹 채팅
    - 소켓연결 진행중

---
+++ Chat/src/main/java/com/project/chat/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.project.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+//                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub"); //서버가 메시지를 수신할 경로
+        registry.enableSimpleBroker("/sub"); //서버가 메시지를 클라이언트로 broadcast 하거나 클라이언트가 메시지를 subscribe 할 때 사용하는 prefix를 설정
+    }
+}
